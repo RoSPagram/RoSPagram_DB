01196e8330e8deb341852458635886e7c118f6f1
rdg1029 <wcle3456@gmail.com>
2024-12-14 16:03:02 +0900

    Add query

---
+++ functions/set_last_login.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION public.set_last_login(user_id text) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+begin
+  update users set last_login = current_date where id = user_id;
+end;
+$$;
+
+
+ALTER FUNCTION public.set_last_login(user_id text) OWNER TO postgres;
