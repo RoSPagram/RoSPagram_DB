4eabc7512ce582f3a0cc8c2c6e4275e09deed124
rdg1029 <wcle3456@gmail.com>
2024-12-19 10:41:49 +0900

    Add queries

---
+++ functions/delete_finished_matches.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION public.delete_finished_match(from_id text, to_id text) RETURNS void
+    LANGUAGE plpgsql
+    AS $$
+begin
+  delete from match where "from" = from_id and "to" = to_id and respond != 0;
+end;
+$$;
+
+ALTER FUNCTION public.delete_finished_match(from_id text, to_id text) OWNER TO postgres;
---
+++ tables/user_tokens.sql
@@ -0,0 +1,7 @@
+CREATE TABLE public.user_tokens (
+    id text not null,
+    count smallint not null default '5'::smallint,
+    last_updated timestamp without time zone null,
+    constraint users_token_pkey primary key (id),
+    constraint users_token_id_fkey foreign key (id) references users (id)
+);
