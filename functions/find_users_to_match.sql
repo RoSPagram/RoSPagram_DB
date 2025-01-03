064a2277aa11d6ef9a2d7c1d6c3bc08642578d11
rdg1029 <wcle3456@gmail.com>
2025-01-03 21:35:27 +0900

    Update query

--- functions/find_users_to_match.sql
+++ functions/find_users_to_match.sql
@@ -4,7 +4,8 @@ CREATE OR REPLACE FUNCTION public.find_users_to_match(sender_id text) RETURNS SE
   begin
     return query
       select * from get_random_users(sender_id)
-      where id not in (select "from" from match)
+      where id not in (select "from" from match where "to" = sender_id)
+      and id not in (select "to" from match where "from" = sender_id)
       limit 20;
   end;
 $$;
