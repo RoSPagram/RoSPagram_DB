e327b399bed8c36f7c3b0ad4145f6d87f0796437
rdg1029 <wcle3456@gmail.com>
2025-01-28 12:05:11 +0900

    Fix query

--- functions/set_win_loss.sql
+++ functions/set_win_loss.sql
@@ -2,7 +2,8 @@ CREATE OR REPLACE FUNCTION public.set_win_loss(winner_id text, loser_id text) RE
     LANGUAGE plpgsql
     AS $$
   begin
-    update users set win = win + 1, gem = gem + 1, xp = xp + 1 where id = winner_id;
+    update users set win = win + 1, xp = xp + 1 where id = winner_id;
+    update user_gems set gem = gem + 1 where id = winner_id;
     update users set loss = loss + 1 where id = loser_id;
     call update_score(winner_id);
     call update_score(loser_id);
